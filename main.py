642b49c5d42d8a8479e87721b3c721929d4209b3
natejonesiii <15nate.jones@gmail.com>
2024-08-19 17:30:04 -0400

    Created prohect base, game window that is closable running, black background set

---
+++ main.py
@@ -0,0 +1,24 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+from pygame.locals import *
+	
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+def main():
+    #Game loop begins
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            else:
+                screen.fill((0, 0, 0))
+                pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
