b73da099ef39deaff44cff77dd0d5b817b32fef5
natejonesiii <15nate.jones@gmail.com>
2024-08-19 18:37:11 -0400

    Player movement left and right work smoothly

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ from constants import *
 from pygame.locals import *
 from player import Player
  
-pygame.init()
 
-FPS = 120
-FramePerSec = pygame.time.Clock()
-dt = 0
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 def main():
+    pygame.init()
+
+    FPS = 120
+    FramePerSec = pygame.time.Clock()
+    dt = 0
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     #Game loop begins
     while True:
         for event in pygame.event.get():
@@ -23,6 +24,7 @@ def main():
                 return
             
         screen.fill((0, 0, 0))
+        player.update(dt)
         player.draw(screen)
         
         pygame.display.flip()
--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 import pygame
 from colors import DEFAULT
 from circleshape import CircleShape
@@ -26,3 +26,14 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, DEFAULT, self.triangle(), 2)
+    
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+        
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
